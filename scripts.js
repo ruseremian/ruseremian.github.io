db7c30bf57d421a3aaa6cd8ff91ae2e0e5b05f11
Ruslan <129394569+ruseremian@users.noreply.github.com>
2023-07-30 17:32:47 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -15,10 +15,28 @@ AOS.init({
 const dynamicText = document.getElementById('dynamic-text');
 const text = ["Hello, I'm Ruslan!", "Welcome to my website!", "I'm a Data Analyst!", "Let's explore my work!"];
 let i = 0;
+let isAdding = true;
+let currentText = "";
 
 function changeText() {
-    dynamicText.innerHTML = text[i];
-    i = (i + 1) % text.length;
+    if (isAdding) {
+        if (currentText.length < text[i].length) {
+            currentText = text[i].substring(0, currentText.length + 1);
+        } else {
+            isAdding = false;
+            setTimeout(changeText, 2000); // Wait for 2 seconds before backspacing
+            return;
+        }
+    } else {
+        if (currentText.length > 0) {
+            currentText = text[i].substring(0, currentText.length - 1);
+        } else {
+            isAdding = true;
+            i = (i + 1) % text.length;
+        }
+    }
+    dynamicText.innerHTML = currentText;
+    setTimeout(changeText, isAdding ? 200 : 100); // Slower when adding characters, faster when backspacing
 }
 
-setInterval(changeText, 2000); // Change text every 2 seconds
+changeText();
