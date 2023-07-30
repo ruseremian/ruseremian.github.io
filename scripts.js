66dfe95b5b1f28bfd64567aa8c2a9c7a47773aff
Ruslan <129394569+ruseremian@users.noreply.github.com>
2023-07-30 18:15:14 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,43 +1,35 @@
-var sidebar = document.getElementById('sidebar');
+window.onload = function() {
+    const dynamicText = document.getElementById('dynamic-text');
+    const text = ["Hello, I'm Ruslan!", "Welcome to my website!", "I'm a Data Analyst!", "Let's explore my work!"];
+    let i = 0;
+    let isAdding = true;
+    let currentText = "";
 
-sidebar.addEventListener('mouseover', function() {
-    sidebar.classList.add('expanded');
-});
-
-sidebar.addEventListener('mouseout', function() {
-    sidebar.classList.remove('expanded');
-});
-
-const dynamicText = document.getElementById('dynamic-text');
-const text = ["Hello, I'm Ruslan!", "Welcome to my website!", "Let's explore my work!"];
-let i = 0;
-let isAdding = true;
-let currentText = "";
-
-function changeText() {
-    if (isAdding) {
-        if (currentText.length < text[i].length) {
-            currentText = text[i].substring(0, currentText.length + 1);
+    function changeText() {
+        if (isAdding) {
+            if (currentText.length < text[i].length) {
+                currentText = text[i].substring(0, currentText.length + 1);
+            } else {
+                isAdding = false;
+                setTimeout(changeText, 2000); // Wait for 2 seconds before backspacing
+                return;
+            }
         } else {
-            isAdding = false;
-            setTimeout(changeText, 2000); // Wait for 2 seconds before backspacing
-            return;
+            if (currentText.length > 0) {
+                currentText = text[i].substring(0, currentText.length - 1);
+            } else {
+                isAdding = true;
+                i = (i + 1) % text.length;
+            }
         }
-    } else {
-        if (currentText.length > 0) {
-            currentText = text[i].substring(0, currentText.length - 1);
-        } else {
-            isAdding = true;
-            i = (i + 1) % text.length;
-        }
+        dynamicText.innerHTML = currentText;
+        setTimeout(changeText, isAdding ? 200 : 100); // Slower when adding characters, faster when backspacing
     }
-    dynamicText.innerHTML = currentText;
-    setTimeout(changeText, isAdding ? 200 : 100); // Slower when adding characters, faster when backspacing
+
+    changeText();
+    
+    // Initializing AOS
+    AOS.init({
+        duration: 1200, // You can adjust the animation duration here
+    });
 }
-
-changeText();
-
-// Initializing AOS
-AOS.init({
-    duration: 1200, // You can adjust the animation duration here
-});
